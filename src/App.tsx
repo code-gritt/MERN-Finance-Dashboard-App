8a2aa57b0d6b1b5c9d508daf270a6c74a35db1e6
gokul <gokulchandan24@gmail.com>
2024-10-14 00:22:19 +0530

    building navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { Box } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./scenes/navbar";
+import { themeSettings } from "./theme";
+
 function App() {
+  const theme = useMemo(() => createTheme(themeSettings), []);
   return (
     <>
-      <div>Gokul</div>
+      <div className="app">
+        <BrowserRouter>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+              <Navbar />
+            </Box>
+          </ThemeProvider>
+        </BrowserRouter>
+      </div>
     </>
   );
 }
